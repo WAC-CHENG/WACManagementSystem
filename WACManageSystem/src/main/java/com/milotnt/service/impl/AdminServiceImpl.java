5cfaf28d946af4b419b2556c6687c6c9d2e282a5
Cheng Zhan <cheng@usawac.com>
2023-11-09 11:30:09 -0500

    modify comments

--- WACManageSystem/src/main/java/com/milotnt/service/impl/AdminServiceImpl.java
+++ WACManageSystem/src/main/java/com/milotnt/service/impl/AdminServiceImpl.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
-/*管理员登录模块业务层实现类*/
+/*Administrator login module business layer implementation class*/
 @Service
 public class AdminServiceImpl implements AdminService {
 
